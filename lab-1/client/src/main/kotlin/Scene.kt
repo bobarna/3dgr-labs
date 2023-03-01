fdbafd07fa964c56c8c90ac925f2235441e02340
bobarna <barnabas.borcsok@gmail.com>
2023-03-01 22:49:50 +0100

    Add lab 1 initial code

---
+++ lab-1/client/src/main/kotlin/Scene.kt
@@ -0,0 +1,17 @@
+import org.w3c.dom.HTMLCanvasElement
+import org.khronos.webgl.WebGLRenderingContext as GL //# GL# we need this for the constants declared ˙HUN˙ a constansok miatt kell
+
+class Scene (
+  val gl : WebGL2RenderingContext){
+
+  fun resize(canvas : HTMLCanvasElement) {
+    gl.viewport(0, 0, canvas.width, canvas.height)//#viewport# tell the rasterizer which part of the canvas to draw to ˙HUN˙ a raszterizáló ide rajzoljon
+  }
+
+  @Suppress("UNUSED_PARAMETER")
+  fun update(keysPressed : Set<String>) {
+    gl.clearColor(0.3f, 0.0f, 0.3f, 1.0f)//## red, green, blue, alpha in [0, 1]
+    gl.clearDepth(1.0f)//## will be useful in 3D ˙HUN˙ 3D-ben lesz hasznos
+    gl.clear(GL.COLOR_BUFFER_BIT or GL.DEPTH_BUFFER_BIT)//#or# bitwise OR of flags
+  }
+}
